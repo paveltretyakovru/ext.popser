f90c5d7258497efcfedb8d9754a50782615f8e03
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-01-09 12:52:54 +0300

    Настроены события autocomplete под rivetsjs

--- source/js/views/serial.js
+++ source/js/views/serial.js
@@ -25,6 +25,7 @@ class Serial extends Backbone.View{
 		this.model 		= this.app.model;
 		this.$settings 	= $('.js-serial-settings');
 		
+		// Подготавливаем список сериалов для автоформы
 		this.prepareSerialsList();
 	}
 
@@ -37,7 +38,13 @@ class Serial extends Backbone.View{
 			if( this.model.isNew() ){ $('.js-serial-settings').css('display' , 'block'); }
 			
 			this.$el.show( 300 , () => {
-				$( "input[name=serial-title]" ).autocomplete({ source: this.serialsList });
+
+				// Вешаем автофому на инпут с заголовком сериала
+				$( "input[name=serial-title]" ).autocomplete({
+					source 	: this.serialsList ,
+					// Риветс не видет изменения внесенные плагином autocomplete
+					select 	: ( event , ui ) => { this.model.set( 'title' , ui.item.value ); }
+				});
 			});
 			
 			this.binding = rivets.bind( this.el , { model : this.model } );
@@ -45,7 +52,11 @@ class Serial extends Backbone.View{
 		return this;
 	}
 
-	// Событие отрабатывает по клику - Сохранить (сериал при создании)
+	/**
+	 * Событие отрабатывает по клику - Сохранить (сериал при создании)
+	 * @param  {jQuery event } e standart jquery event object
+	 * @return {void}   сохраняет созданный сериал в каталог пользователя
+	 */
 	saveSerial( e ){
 
 		if( !this.model.get('_token') ){ this.model.set('_token' , this.app.token ); }
@@ -55,11 +66,13 @@ class Serial extends Backbone.View{
 				console.error('Ошибка запроса');
 			})
 			.done( () =>{
-				this.render({ model : this.model });
-				console.log('Created' , this.model.isNew() , this.model.toJSON() );
-			});
+				this.render({ model : this.model });});
 	}
 
+	/**
+	 * Подготавливает загруженный список сериалов к виду для autocomplete
+	 * @return {void} заполняет массива this.serialsList значениями
+	 */
 	prepareSerialsList(){
 		if( this.app.Serials ){
 			this.serialsList = _.map( this.app.Serials , ( obj ) => {
@@ -68,8 +81,6 @@ class Serial extends Backbone.View{
 					value : obj.title
 				};
 			})
-
-			console.log('SERIAL LIST' , this.serialsList );
 		}
 	}
 };
